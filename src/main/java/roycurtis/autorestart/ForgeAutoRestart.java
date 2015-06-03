2c08c72acb1f3a3c89776f217272d232961be346
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2015-06-03 14:57:13 +0100

    - added config reading of schedule time and message
    - implemented restart task that kicks all users
    - cleaned up gradle file and code

--- src/main/java/roycurtis/autorestart/ForgeAutoRestart.java
+++ src/main/java/roycurtis/autorestart/ForgeAutoRestart.java
@@ -10,8 +10,10 @@ import net.minecraftforge.common.config.Configuration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Timer;
 
 @Mod(
@@ -27,11 +29,16 @@ public class ForgeAutoRestart
     static final String  MODID   = "ForgeAutoRestart";
     static final String  VERSION = "1.0.0";
     static final Logger  LOGGER  = LogManager.getFormatterLogger(MODID);
-    static final Boolean DEBUG   = Boolean.parseBoolean( System.getProperty("debug", "false") );
+
+    @Mod.Instance(MODID)
+    static ForgeAutoRestart INSTANCE;
 
     Configuration config;
+    Timer         timer;
 
-    Timer timer = new Timer("Forge AutoRestart timer");
+    int    cfgHour    = 6;
+    int    cfgMinute  = 0;
+    String cfgMessage = "Scheduled server shutdown";
 
     @EventHandler
     @SideOnly(Side.CLIENT)
@@ -46,25 +53,33 @@ public class ForgeAutoRestart
     public void serverPreInit(FMLPreInitializationEvent event)
     {
         config = new Configuration( event.getSuggestedConfigurationFile() );
+        timer  = new Timer("Forge AutoRestart timer");
+
+        cfgHour    = config.getInt("Hour", "Schedule", cfgHour, 0, 23, "");
+        cfgMinute  = config.getInt("Minute", "Schedule", cfgMinute, 0, 59, "");
+        cfgMessage = config.getString("Message", "Shutdown", cfgMessage, "");
+
+        config.save();
     }
 
     @EventHandler
     @SideOnly(Side.SERVER)
     public void init(FMLInitializationEvent event)
     {
-        Calendar restartAt = Calendar.getInstance();
-        restartAt.set(Calendar.HOUR_OF_DAY, 6);
-        restartAt.set(Calendar.MINUTE,      30);
-        restartAt.set(Calendar.SECOND,      0);
+        DateFormat  dateFormat = new SimpleDateFormat("HH:mm:ss dd-MMMM-yyyy");
+        RestartTask task       = new RestartTask();
+        Calendar    restartAt  = Calendar.getInstance();
+        restartAt.set(Calendar.HOUR_OF_DAY, cfgHour);
+        restartAt.set(Calendar.MINUTE, cfgMinute);
 
+        // Adjust for when current time surpasses restart schedule
+        // (e.g. if restart time is 07:00 and current time is 13:21)
         if ( restartAt.before( Calendar.getInstance() ) )
-        {
             restartAt.roll(Calendar.DATE, true);
-            LOGGER.debug("Rolling restart date forward");
-        }
 
-        timer.schedule(new RestartTask(), restartAt.getTime());
-        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss dd-MMMM-yyyy");
-        LOGGER.info("Automatic restart at %s (%d)", formatter.format(restartAt.getTime()), restartAt.getTimeInMillis());
+        Date restartAtDate = restartAt.getTime();
+
+        timer.schedule(task, restartAtDate);
+        LOGGER.info( "Next automatic restart: %s", dateFormat.format(restartAtDate) );
     }
 }
--- src/main/java/roycurtis/autorestart/RestartTask.java
+++ src/main/java/roycurtis/autorestart/RestartTask.java
@@ -1,5 +1,8 @@
 package roycurtis.autorestart;
 
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+
 import java.util.TimerTask;
 
 public class RestartTask extends TimerTask
@@ -7,6 +10,15 @@ public class RestartTask extends TimerTask
     @Override
     public void run()
     {
-        ForgeAutoRestart.LOGGER.fatal("TIMER FIRED");
+        ForgeAutoRestart instance = ForgeAutoRestart.INSTANCE;
+        MinecraftServer  server   = MinecraftServer.getServer();
+
+        for (Object value : server.getConfigurationManager().playerEntityList)
+        {
+            EntityPlayerMP player = (EntityPlayerMP) value;
+            player.playerNetServerHandler.kickPlayerFromServer(instance.cfgMessage);
+        }
+
+        server.initiateShutdown();
     }
 }
